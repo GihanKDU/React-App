e6b9c7599c3caf9d0d4416a41a61c775b90c13b1
yasiru <yasiru@zmessenger.com>
2020-11-13 21:22:56 +0530

    timer

--- Library-client-master/src/pages/paper.js
+++ Library-client-master/src/pages/paper.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from 'next/router';
 // import Footer from '../components/Footer/Footer';
-
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import Breadcome from '../components/Breadcome';
 import Exam from '../components/Exam'
 
 
 const Paper = () => {
-    
-
-    return(
+    const [isPlaying, setIsPlaying] = useState(true);
+   
+    const children = ({ remainingTime }) => {
+        const minutes = Math.floor(remainingTime / 60)
+        const seconds = remainingTime % 60
+      
+        return `${minutes}:${seconds}`
+      }
+    return (
         <div className="">
-            
-            <Breadcome/>
-            <Exam/>
-            
-           
-       {/* <Footer/> */}
-       </div>
+            <CountdownCircleTimer
+                isPlaying={isPlaying}
+                duration={3600}
+                size={70}
+                onComplete={() => {
+                    // return [true]
+                }}
+                colors={[ 
+                    ['#004777', 0.33],
+                    ['#F7B801', 0.33],
+                    ['#A30000', 0.33],
+                ]}
+            >
+                {children}
+            </CountdownCircleTimer>
+            <Breadcome />
+            <Exam />
+
+
+            {/* <Footer/> */}
+        </div>
     )
 }
 
